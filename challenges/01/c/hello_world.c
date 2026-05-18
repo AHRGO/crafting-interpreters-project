16e9042a58e3f405558c0f2e43cbe13f06dabee6
Victor Farias <60015667+AHRGO@users.noreply.github.com>
2026-05-18 14:41:01 +0000

    adds new methods to insert, find last, create and insert and a new test method

--- challenges/01/c/hello_world.c
+++ challenges/01/c/hello_world.c
@@ -9,6 +9,16 @@ Write functions to insert, find, and delete items from it.
 Test them.
 */
 
+
+/**
+ * 
+ * validating if the code has memory leaks:
+ * 1. using the valgrind tool:
+ * -    valgrind --leak-check=full ./program
+ * 
+ * 2. using address sanitizer
+ * -    gcc -fsanitize=address -g program.c -o program
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -23,12 +33,18 @@ typedef struct Node {
 
 Node *create_node(const char *text);
 Node *find_node(Node *head, const char *target);
+Node *insert_node(Node *list_node, Node *node_to_insert);
+Node *find_last_node(Node *node);
 void delete_node(Node **head, Node *node);
+
+void create_and_insert_node(Node *node_in_the_list ,const char *text);
 
 void free_node(Node *node);
 void cleanup_mem(Node *first);
+void node_freedom_cry(Node *node);
 
 void test_nodes();
+void test_nodes_secnd_ed();
 
 
 Node *create_node(const char *text) {
@@ -67,6 +83,33 @@ Node *find_node(Node *head, const char *target) {
     return NULL;
 }
 
+Node *insert_node(Node *list_node, Node *node_to_insert) {
+    if (node_to_insert == NULL) {
+        return NULL;
+    }
+
+    Node *tail_node = find_last_node(list_node);
+
+    // tail_node->next = malloc(sizeof(Node));
+    tail_node->next = node_to_insert;
+
+    return tail_node->next;
+}
+
+Node *find_last_node(Node *node) {
+    //stop condition
+    if (node->next == NULL) {
+        return node;
+    }
+
+    find_last_node(node->next);
+}
+
+void create_and_insert_node(Node *node_in_the_list ,const char *text) {
+    Node *node_to_be_inserted = create_node(text);
+    insert_node(node_in_the_list, node_to_be_inserted);
+}
+
 void test_nodes() {
     Node *first = create_node("Hello");
     Node *second = create_node("World");
@@ -83,7 +126,33 @@ void test_nodes() {
     printf("%s ", first->next->text);
     printf("%s", first->next->next->text);
 
+    printf("\n\n finding the last node ... \n\n");
+
+    Node *tail_node = find_last_node(first);
+    printf("\nTail node's message is:: %s\n", tail_node->text);
+
     cleanup_mem(first);
+}
+
+void test_nodes_secnd_ed() {
+    Node *first = create_node("Hello");
+    create_and_insert_node(first, "Marvelous");
+    create_and_insert_node(first, "World");
+    create_and_insert_node(first, "!");
+    create_and_insert_node(first, "Yeahhaw");
+
+    // Node *current_node = malloc(sizeof(Node));
+    Node *current_node = first;
+    while (current_node->next != NULL) {
+        printf("%s ", current_node->text);
+        current_node = current_node->next;
+    }
+
+    printf("\n");
+
+    // cleanup_mem(first);
+    free_all_the_nodes(first);
+    // node_freedom_cry(first);
 }
 
 
@@ -129,6 +198,15 @@ void free_all_the_nodes(Node *node) {
 
 }
 
+void node_freedom_cry(Node *node) {
+    Node *tail_node = find_last_node(node);
+    Node *updated_tail_node;// = tail_node->prev;        
+    while(tail_node->prev != NULL) {
+        updated_tail_node = tail_node->prev;
+        free(tail_node);
+    }
+}
+
 void free_node(Node *node) {
     free(node->text);
     free(node);
@@ -142,8 +220,8 @@ void free_node(Node *node) {
 int main() {
     // printf("Hello World!\n");
 
-    test_nodes();
-    // cleanup_mem();
+    // test_nodes();
+    test_nodes_secnd_ed();
 
     return 0;
 }
